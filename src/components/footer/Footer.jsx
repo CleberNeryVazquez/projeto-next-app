79e2f5c45f0ba7053b86753b29832c9faf0d323e
CleberNeryVazquez <clebernery19@gmail.com>
2023-08-17 03:52:51 -0300

    Criado rotas e componentes navbar e footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <div>2023 - Cleber Nery Vazquez</div>
+    <div></div>  
+    </div>
+  )
+}
+
+export default Footer
