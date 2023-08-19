4574cb5714f254e2f36cefdf26f3b20888cd7bd2
CleberNeryVazquez <clebernery19@gmail.com>
2023-08-18 22:24:38 -0300

    Comecando a sessao sobre

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
-
+import styles from './page.module.css'
 const Sobre = () => {
   return (
-    <div>Sobre</div>
+    <div>
+      <div className={styles.imgContainer}
+      >
+        <Image src='' fill={true} alt='' className={styles.img}/>
+      </div>
+      <div className={styles.imgText}>
+        <h1 className={styles.imgTitulo}></h1>
+        <h2 className={styles.imgDesc}></h2>
+      </div>
+    </div>
   )
 }
 
