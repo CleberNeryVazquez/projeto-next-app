6debd2a167bc73d3a2b5ef6d0e53fd245622c40b
CleberNeryVazquez <clebernery19@gmail.com>
2023-08-24 03:10:44 -0300

    Testando blog/posts com api jsonPlaceholder

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
+import {notFound} from 'next/navigation'
 
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    return notFound()
+  }
+ 
+  return res.json()
+}
 
-const BlogPost = () => {
+const BlogPost = async ({params}) => {
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Este é o titulo do artigo</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eaque repudiandae maiores quibusdam veniam ipsa et sunt incidunt, autem suscipit voluptatibus. Cumque aspernatur sunt, culpa cum dicta laboriosam doloribus nihil.</p>
           <div className={styles.autor}>
             <Image src='' alt='' width={40} height={40} className={styles.avatar} />
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,10 +2,24 @@ import Link from 'next/link'
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
-const Blog = () => {
+
+
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error('Falha no ao buscar dados')
+  }
+ 
+  return res.json()
+}
+const Blog = async () => {
+  const data = await getData()
   return (
     <div className={styles.mainContainer}>
-      <Link href='/blog/testid' className={styles.container} >
+      {data.map(item => (
+      <Link href='/blog/testid' className={styles.container} key={item.id}>
         <div className={styles.imageContainer}>
           <Image
           src=''
@@ -15,38 +29,11 @@ const Blog = () => {
           className={styles.img}/>
         </div>
         <div className={styles.content}>
-          <h1 className={styles.title}>Titulo do artigo, não muito longo</h1>
+          <h1 className={styles.title}>{item.title}</h1>
           <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quasi hic exercitationem architecto, omnis consectetur consequuntur veniam explicabo dolores praesentium quae saepe, error ducimus, voluptatem nesciunt nostrum rem eveniet! Aperiam?</p>
         </div>
       </Link>
-      <Link href='/blog/testid' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-          src=''
-          alt=''
-          width={400}
-          height={250}
-          className={styles.img}/>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Titulo do artigo, não muito longo</h1>
-          <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quasi hic exercitationem architecto, omnis consectetur consequuntur veniam explicabo dolores praesentium quae saepe, error ducimus, voluptatem nesciunt nostrum rem eveniet! Aperiam?</p>
-        </div>
-      </Link>
-      <Link href='/blog/testid' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-          src=''
-          alt=''
-          width={400}
-          height={250}
-          className={styles.img}/>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Titulo do artigo, não muito longo</h1>
-          <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quasi hic exercitationem architecto, omnis consectetur consequuntur veniam explicabo dolores praesentium quae saepe, error ducimus, voluptatem nesciunt nostrum rem eveniet! Aperiam?</p>
-        </div>
-      </Link>
+      ))}
     </div>
   )
 }
